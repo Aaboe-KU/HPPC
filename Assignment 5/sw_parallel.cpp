2f27443a540ccf92c966a3d2785e08ff7f4ae05d
Emilie <emilie.hbv17@gmail.com>
2024-03-06 14:56:26 +0100

    bkjhgcffr

--- Assignment 5/sw_parallel.cpp
+++ Assignment 5/sw_parallel.cpp
@@ -66,7 +66,9 @@ public:
     grid_t v{}; // The speed in the vertical direction.
     grid_t e{}; // The water elevation.
     Water() {
+        #pragma acc parallel loop gang copy(e)
         for (size_t i = 1; i < NY - 1; ++i) 
+        #pragma acc loop vector
         for (size_t j = 1; j < NX - 1; ++j) {
             real_t ii = 100.0 * (i - (NY - 2.0) / 2.0) / NY;
             real_t jj = 100.0 * (j - (NX - 2.0) / 2.0) / NX;
@@ -91,6 +93,7 @@ void to_file(const std::vector<grid_t> &water_history, const std::string &filena
  * @param shape  The shape of data including the ghost lines.
  */
 void exchange_horizontal_ghost_lines(grid_t& data) {
+    #pragma acc parallel loop vector
     for (uint64_t j = 0; j < NX; ++j) {
         data[0][j]      = data[NY-2][j]; 
         data[NY-1][j]   = data[1][j];
@@ -103,6 +106,7 @@ void exchange_horizontal_ghost_lines(grid_t& data) {
  * @param shape  The shape of data including the ghost lines.
  */
 void exchange_vertical_ghost_lines(grid_t& data) {
+    #pragma acc parallel loop vector
     for (uint64_t i = 0; i < NY; ++i) {
         data[i][0] = data[i][NX-2];
         data[i][NX-1] = data[i][1];
@@ -114,22 +118,29 @@ void exchange_vertical_ghost_lines(grid_t& data) {
  * @param w The water world to update.
  */
 void integrate(Water &w, const real_t dt, const real_t dx, const real_t dy, const real_t g) {
+    #pragma acc data copy(w) 
+    {
     exchange_horizontal_ghost_lines(w.e);
     exchange_horizontal_ghost_lines(w.v);
     exchange_vertical_ghost_lines(w.e);
     exchange_vertical_ghost_lines(w.u);
 
-
+    #pragma acc parallel loop gang
     for (uint64_t i = 0; i < NY - 1; ++i) 
+    #pragma acc loop vector
     for (uint64_t j = 0; j < NX - 1; ++j) {
         w.u[i][j] -= dt / dx * g * (w.e[i][j+1] - w.e[i][j]);
         w.v[i][j] -= dt / dy * g * (w.e[i + 1][j] - w.e[i][j]);
     }
 
+
+    #pragma acc parallel loop gang
     for (uint64_t i = 1; i < NY - 1; ++i) 
+    #pragma acc loop vector
     for (uint64_t j = 1; j < NX - 1; ++j) {
         w.e[i][j] -= dt / dx * (w.u[i][j] - w.u[i][j-1])
                    + dt / dy * (w.v[i][j] - w.v[i-1][j]);
+    }
     }
 }
 
